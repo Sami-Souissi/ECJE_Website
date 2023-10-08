cf79367d35a427b7eda035d6c7b2f653a926b5bb
“Sami-Souissi” <samisouissi708@gmail.com>
2023-10-08 23:37:16 +0100

    EcjeApp_FULL_APLPHA_V1.5

---
+++ contactlinks/assets/config.js
@@ -0,0 +1,47 @@
+let config = {
+  "Title": "ECJE - Links",
+  "Name": "ENET'COM JUNIOR ENTREPRISE",
+  "Description": "GO BIG OR GO HOME",
+  "links": [
+    // {
+    //   "Title": "Website",
+    //   "URL": "",
+    //   "icon_classes": "fas fa-link",
+    // },
+    {
+      "Title": "Facebook",
+      "URL": "https://www.facebook.com/enetcomje/",
+      "icon_classes": "fab fa-facebook",
+    },
+    {
+      "Title": "Instagram",
+      "URL": "https://www.instagram.com/enetcomjunior/",
+      "icon_classes": "fab fa-instagram"
+    },
+    // {
+    //   "Title": "PC Games",
+    //   "URL": "",
+    //   "icon_classes": "fab fa-itch-io"
+    // },
+    // {
+    //   "Title": "Blog",
+    //   "URL": "blog-test",
+    //   "icon_classes": "fas fa-blog"
+    // },
+    // {
+    //   "Title": "Twitter",
+    //   "URL": "https://twitter.com/Harindu_Fonseka",
+    //   "icon_classes": "fab fa-twitter"
+    // },
+    {
+      "Title": "LinkedIn",
+      "URL": "https://www.linkedin.com/in/junior-enetcom-85129b153/",
+      "icon_classes": "fab fa-linkedin"
+    },
+    {
+      "Title": "Youtube",
+      "URL": "https://www.youtube.com/c/ENETComJuniorEntreprise",
+      "icon_classes": "fab fa-youtube"
+    },
+  ]
+}
